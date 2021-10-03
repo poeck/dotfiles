21d569e8f575d511d47156539cd5c56296c66cb9
Paul Koeck <paul@koeck.dev>
2021-10-03 19:46:35 +0200

    Cleaned up doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,23 +1,51 @@
+;; -----------------
+;;       Info
+;; -----------------
+
 (setq user-full-name "Paul Koeck"
       user-mail-address "paul@koeck.dev")
+
+;; -----------------
+;;     Packages
+;; -----------------
+
+(use-package emmet-mode)
+(use-package prettier-js)
+(use-package dart-mode)
+(use-package web-mode)
+
+;; -----------------
+;;       Style
+;; -----------------
+
 (setq doom-theme 'doom-one)
 (setq org-directory "~/org/")
 (setq display-line-numbers-type "relative")
-
-;; Window settings 
 (setq default-frame-alist '((undecorated . t)))
+
+;; -----------------
+;;       Font
+;; -----------------
 
 (setq doom-font (font-spec :family "CascadiaCode Nerd Font" :size 16)
       doom-variable-pitch-font (font-spec :family "Noto Sans" :size 16)
       doom-big-font (font-spec :family "CascadiaCode Nerd Font" :size 24))
 
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-(add-to-list 'default-frame-alist '(fullscreen . fullheight))
+;; -----------------
+;;      Window
+;; -----------------
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
-(add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(fullscreen . fullheight))
+
+
+;; -----------------
+;;     Web-Mode
+;; -----------------
 
 (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
@@ -27,60 +55,42 @@
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
 
-(use-package emmet-mode) 
+;; Hooks
+(add-hook 'web-mode-hook 'prettier-js-mode)
 (add-hook 'web-mode-hook  'emmet-mode)
 
-(use-package prettier-js) 
-(add-hook 'web-mode-hook 'prettier-js-mode)
-; (add-hook 'after-init-hook #'global-prettier-mode)
+;; -----------------
+;;     Flutter
+;; -----------------
 
-(use-package dart-mode
-  :hook (dart-mode . flutter-test-mode))
+(add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
 
 (use-package flutter
   :after dart-mode
-  :bind (:map dart-mode-map
-              ("C-M-x" . #'flutter-run-or-hot-reload))
-  :custom
-  (flutter-sdk-path "/snap/bin/flutter/"))
+  :bind (
+         :map dart-mode-map
+         ("C-M-x" . #'flutter-run-or-hot-reload)
+        )
+  :custom (flutter-sdk-path "/snap/bin/flutter/")
+)
+
+;; -----------------
+;;   Multi-VTerm
+;; -----------------
 
 (use-package multi-vterm
 	:config
 	(add-hook 'vterm-mode-hook
-			(lambda ()
-			(setq-local evil-insert-state-cursor 'box)
-			(evil-insert-state)))
-	(define-key vterm-mode-map [return]                      #'vterm-send-return)
-
+	(lambda ()
+	(setq-local evil-insert-state-cursor 'box)
+	(evil-insert-state)))
+	(define-key vterm-mode-map [return] #'vterm-send-return)
 	(setq vterm-keymap-exceptions nil)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-e")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-f")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-a")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-v")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-b")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-w")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-u")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-d")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-n")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-m")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-p")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-j")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-k")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-r")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-t")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-g")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-c")      #'vterm--self-insert)
-	(evil-define-key 'insert vterm-mode-map (kbd "C-SPC")    #'vterm--self-insert)
-	(evil-define-key 'normal vterm-mode-map (kbd "C-d")      #'vterm--self-insert)
-	(evil-define-key 'normal vterm-mode-map (kbd ",c")       #'multi-vterm)
-	(evil-define-key 'normal vterm-mode-map (kbd ",n")       #'multi-vterm-next)
-	(evil-define-key 'normal vterm-mode-map (kbd ",p")       #'multi-vterm-prev)
-	(evil-define-key 'normal vterm-mode-map (kbd "i")        #'evil-insert-resume)
-	(evil-define-key 'normal vterm-mode-map (kbd "o")        #'evil-insert-resume)
-	(evil-define-key 'normal vterm-mode-map (kbd "<return>") #'evil-insert-resume))
+)
 
-(use-package web-mode
-  :mode (("\\.tsx" . web-mode)
-                ("\\.jsx" . web-mode)))
+
+;; -----------------
+;;       Maps
+;; -----------------
 
 (map! :i "C-S-v" #'clipboard-yank)
